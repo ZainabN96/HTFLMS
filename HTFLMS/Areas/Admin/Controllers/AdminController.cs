06c5a528358867beb69a6cf7566c609c2b34e60e
sehrishAun <sehrishaunn@gmail.com>
2026-04-08 17:03:51 +0500

    Admin Frontend screens

---
+++ HTFLMS/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HTFLMS.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminController : Controller
+    {
+        public IActionResult AdminDashboard()
+        {
+            return View();
+        }
+      
+
+    }
+}
